7b7225c85c875ab6a65f9a2597e3907f79e395e1
Navjoth Boddepalli <37272244+navjothbn@users.noreply.github.com>
2020-08-06 17:10:45 +0530

    Add files via upload

---
+++ target_classes.js
@@ -0,0 +1,17 @@
+
+// function my1(){
+
+
+// var img = document.createElement('img'); 
+//             img.src =  'images/chatbot.jpg'; 
+    
+//             document.getElementById('hod').appendChild(img); 
+//             down.innerHTML = "Image Element Added.";  
+// }
+
+TARGET_CLASSES = {
+  0: "Name Of The Building: Architecture department <br>Head Of the Department:Prof-G-Viswanadha-Kumar<br><img src='images/Prof-G-Viswanadha-Kumar.jpg' height='100px' width='100px' style='border-radius:25px'><br>Available courses:B.Arch,B.Planning<br>More Details:<a href='https://www.andhrauniversity.edu.in/college/college-of-engineering/engineering-departments/architecture'>click here for more info</a>",
+  1: "Name Of The Building: AUCE Canteen <br><img src='images/about.jpg' height='100px' width='100px' style='border-radius:25px'><br>Available courses:B.Arch,B.Planning<br>More Details:<a href='https://www.andhrauniversity.edu.in/college/college-of-engineering/engineering-departments/architecture'>click here for more info</a>",
+  2: "Name Of The Building: Electrical  department <br>Head Of the Department:Prof. G.V.Siva Krishna Rao<br><img src='images/GADIVENKASIVAKRISHNARAO.jpg' height='100px' width='100px' style='border-radius:25px'><br>Available courses:B.Tech(EEE),M.Tech(EEE)<br>More Details:<a href='https://www.andhrauniversity.edu.in/college/college-of-engineering/engineering-departments/architecture'>click here for more info</a>",
+};
+
